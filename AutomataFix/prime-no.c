bc23715ddeedf6c85b5875c6c8bfc039b8e66533
pushkarguptaaa <pushkardps8@gmail.com>
2022-09-24 17:47:27 +0530

    prime no.

---
+++ AutomataFix/prime-no.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <math.h>
+int isprime(int n)
+{
+	for(int i=2; i<= sqrt(n); i++){
+        if(n%i==0){
+          return 0;
+        }
+    }
+  return 1;
+}
+void print(int a, int b)
+{
+    int i;
+    for(i=a;i<=b;i++)
+    {
+        if(isprime(i)==1)
+            printf("%d ",i);
+    }
+}
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    print(a,b);
+    return 0;
+}
