47d2828ac2a851f32398e4092d671859e8c6cf38
pushkarguptaaa <pushkardps8@gmail.com>
2022-10-02 22:19:24 +0530

    symbol pattern

---
+++ Automata/symbol-pattern2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int r, sp, star, n;
+  cin>>n;
+  for(r=n; r>=2; r--){
+     for(sp=1; sp<=n-r; sp++) {
+       cout<<" ";
+     }
+     for(star=1; star<=2*r-1; star++) {
+       cout<<"*";
+     }
+    cout<<"\n";
+  }
+  for(r=1; r<=n; r++){
+   for(sp=1; sp<=n-r; sp++){
+      cout<<" "; 
+   }
+   for(star=1; star<=2*r-1; star++){
+      cout<<"*"; 
+   }
+    cout<<"\n";
+  }
+  
+    return 0;
+}
