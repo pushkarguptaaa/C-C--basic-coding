f15ca4586f4f3da6abec1315939a1c8d9d13a55e
pushkarguptaaa <pushkardps8@gmail.com>
2022-10-21 13:45:06 +0530

    priority queue stl

---
+++ STL/stl5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    // max heap
+    priority_queue<int> maxi;
+    // min heap
+    priority_queue<int, vector<int>, greater<int> > mini;
+
+    maxi.push(3);
+    maxi.push(4);
+    maxi.push(5);
+    maxi.push(1);
+    int n = maxi.size();
+    for(int i=0; i<n; i++){
+        cout<<maxi.top()<<" ";
+        maxi.pop();
+    }
+    cout<<endl;
+
+    mini.push(3);
+    mini.push(4);
+    mini.push(5);
+    mini.push(1);
+
+    int m = mini.size();
+    for(int i=0; i<m; i++){
+        cout<<mini.top()<<" ";
+        mini.pop();
+    }
+    cout<<endl;
+
+    cout<<"Empty-> "<<mini.empty()<<endl;
+
+}
